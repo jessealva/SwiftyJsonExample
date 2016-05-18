5096b553d1b5ae166d74f3fec1ef76be247e6a21
Jesus G. Alva <jalva@us.ibm.com>
2016-05-18 14:25:56 -0500

    moving back to the old version cause the newer version is still broken

--- Package.swift
+++ Package.swift
@@ -2,7 +2,8 @@ import PackageDescription
 
 #if os(Linux)
    let swiftyJsonUrl = "https://github.com/IBM-Swift/SwiftyJSON.git"
-   let swiftyJsonVersion = 7
+   //let swiftyJsonVersion = 7
+   let swiftyJsonVersion = 3
 #else
    let swiftyJsonUrl = "https://github.com/SwiftyJSON/SwiftyJSON.git"
    let swiftyJsonVersion = 2
